0dfb69c6cd72f047de961caa60261abe2fb6ce08
Mykola Lutsenko <Mykola.Lutsenko@dataart.com>
2022-08-06 14:04:43 +0300

    create HUD scene and connect it with Main

--- Main.gd
+++ Main.gd
@@ -7,18 +7,18 @@ var score
 func _ready():
 	randomize()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	
+	$HUD.show_game_over()
 
 
 func _on_MobTimer_timeout():
@@ -46,10 +46,9 @@ func _on_MobTimer_timeout():
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
 
-
 func _on_ScoreTimer_timeout():
 	score += 1
-
+	$HUD.update_score(score)
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
